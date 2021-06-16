3eba796c259514615289325ef36eff00440ab631
Anthony Francis <tonytrack@gmail.com>
2021-06-16 16:29:37 +0100

    update design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
   # get 'pages/index'
+
+  get "post" => "pages#post"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
