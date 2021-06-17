4acf6c4136abaf08ce5a43ed47626a905205b41c
Anthony Francis <tonytrack@gmail.com>
2021-06-17 17:05:50 +0100

    update create new post design

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
 	mount_uploader :cover_image, PostCoverImageUploader
 
 	has_rich_text :body
+
+	# validations
+  	validates :title, presence: true
+  	validates :cover_image, presence: true
+  	validates :body, presence: true
 end
