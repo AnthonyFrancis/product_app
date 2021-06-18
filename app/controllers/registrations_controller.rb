60ddd3f8c4db76710540b12ad72c26c02b71cf3f
Anthony Francis <tonytrack@gmail.com>
2021-06-18 09:21:59 +0100

    general update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -33,7 +33,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    rooms_path
+    root_path
   end
 
   def update_resource(resource, params)
